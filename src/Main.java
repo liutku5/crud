80f3d653bb92f773b47dd847e5a14d48dfd23f21
liutku5 <l1utkuz20@gmail.com>
2024-05-23 15:12:51 +0300

    all the function done

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         ArrayList<String> movielist = new ArrayList();
         Scanner sc = new Scanner(System.in);
         movielist.add("The Matrix");
@@ -12,57 +11,21 @@ public class Main {
         movielist.add("Assassin's Creed");
         movielist.add("X-Men: Days of Future Past");
         while (true) {
-            System.out.println();
-            System.out.println("--------------------------");
-            System.out.println("1. Enter movie name");
-            System.out.println("2. Show movie list");
-            System.out.println("3. Edit movie list");
-            System.out.println("4. Remove movie from the list");
-            System.out.println("5. Close program");
-            System.out.println("--------------------------");
-            System.out.println();
+            printInfoMessege();
             int movie = sc.nextInt();
             sc.nextLine();
             switch (movie) {
                 case 1:
-                    System.out.println(movielist);
-                    System.out.println("Enter movie name.");
-                    String movieinput = sc.nextLine();
-                    if (!movielist.contains(movieinput)) {
-                        movielist.add(movieinput);
-                        System.out.println("The movie " + movieinput + " was added to the list.");
-                    } else {
-                        System.out.println("The movie name already exist.");
-                    }
+                    addMovie(movielist, sc);
                     break;
                 case 2:
-                    for (int i = 0; i < movielist.size(); i++) {
-                        System.out.println(movielist.get(i));
-                    }
+                    printMovies(movielist);
                     break;
                 case 3:
-                    System.out.println(movielist);
-                    System.out.println("Enter new movies name.");
-                    String oldMovie = sc.nextLine();
-                    if (movielist.contains(oldMovie)) {
-                        String newMovie = sc.nextLine();
-                        int choice = movielist.indexOf(oldMovie);
-                        movielist.set(choice, newMovie);
-                        System.out.println("The movie " + oldMovie + " was changed to " + newMovie);
-                    } else {
-                        System.out.println("There is no such movie in the list.");
-                    }
+                    changeMovie(movielist, sc);
                     break;
                 case 4:
-                    System.out.println(movielist);
-                    System.out.println("Enter movies name you wish to remove.");
-                    String movieName = sc.nextLine();
-                    if (movielist.contains(movieName)) {
-                        movielist.remove(movieName);
-                        System.out.println("The movie " + movieName + " was removed from the list.");
-                    } else {
-                        System.out.println("There is no such movie in the list.");
-                    }
+                    removeMovie(movielist, sc);
                     break;
                 case 5:
                     System.exit(1);
@@ -71,5 +34,73 @@ public class Main {
             }
         }
 
+    }
+
+    public static void printInfoMessege() {
+        System.out.println();
+        System.out.println("--------------------------");
+        System.out.println("1. Enter movie name");
+        System.out.println("2. Show movie list");
+        System.out.println("3. Edit movie list");
+        System.out.println("4. Remove movie from the list");
+        System.out.println("5. Close program");
+        System.out.println("--------------------------");
+        System.out.println();
+    }
+
+    public static void addMovie(ArrayList<String> movielist, Scanner sc) {
+        System.out.println(movielist);
+        System.out.println("Enter movie name.");
+        String movieinput = sc.nextLine();
+        if (!movielist.contains(movieinput)) {
+            movielist.add(movieinput);
+            System.out.println("The movie " + movieinput + " was added to the list.");
+        } else {
+            System.out.println("The movie name already exist.");
+        }
+    }
+
+    public static void printMovies(ArrayList<String> movielist) {
+        for (int i = 0; i < movielist.size(); i++) {
+            System.out.println(movielist.get(i));
+        }
+    }
+
+    public static void changeMovie(ArrayList<String> movielist, Scanner sc) {
+        System.out.println(movielist);
+        System.out.println("Enter the movie name you wish to change.");
+        String oldMovie = sc.nextLine();
+        if (movielist.contains(oldMovie)) {
+            System.out.println("Enter new movie name.");
+            String newMovie = sc.nextLine();
+            int choice = movielist.indexOf(oldMovie);
+            movielist.set(choice, newMovie);
+            System.out.println("The movie " + oldMovie + " was changed to " + newMovie);
+        } else {
+            System.out.println("There is no such movie in the list.");
+        }
+    }
+
+    public static void removeMovie(ArrayList<String> movielist, Scanner sc) {
+        System.out.println(movielist);
+        System.out.println("Enter movies name you wish to remove.");
+        String movieName = sc.nextLine();
+        if (movielist.contains(movieName)) {
+            movielist.remove(movieName);
+            System.out.println("The movie " + movieName + " was removed from the list.");
+        } else {
+            System.out.println("There is no such movie in the list.");
+        }
+    }
+
+    public static int intInput(Scanner sc) {
+        while (true) {
+            try {
+                return sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Plese enter a digit");
+                sc.nextLine();
+            }
+        }
     }
 }
