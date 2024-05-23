173839c98136666e0d6aaf5f689e53462b33da74
liutku5 <l1utkuz20@gmail.com>
2024-05-23 11:16:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ArrayList<String> shoppingList = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+    }
+}
